02aa38d63ff09d5fef9f37ac4c150b28e3786351
Antonio Rossi <a.rossi@jayex.com>
2019-12-21 19:36:33 +0100

    bar chart added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { LineChartComponent } from './line-chart/line-chart.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'line', component: LineChartComponent}
+  { path: '', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { ChartsModule } from 'ng2-charts';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LineChartComponent
+    LineChartComponent,
+    HomeComponent,
+    BarChartComponent
   ],
   imports: [
     BrowserModule,
